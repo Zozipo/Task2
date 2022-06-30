eee58864529acd4f8142c10e81e17c68d3d74f07
Mazuruk Nikita <mazuruk220900@gmail.com>
2022-06-30 14:54:28 +0300

    commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// 1
+/*
+let arr = [];
+
+for (let i = 0; i < 10; i++) {
+    arr.push(parseInt(prompt(`Enter ${i+1} mark:`)));
+}
+
+
+
+let temp = true;
+
+while(temp){
+    let choice = parseInt(prompt(`1.Show all marks\n2.Change 1 mark\n3.Avg`))
+    switch (choice) {
+        case 1:
+            arr.forEach(item => {
+                console.log(`${item}`)
+            })
+            break;
+        case 2:
+            let index = parseInt(prompt(`Enter mark index:`));
+            let mark = parseInt(prompt(`Enter mark:`));
+            arr[index] = mark;
+            break;
+        case 3:
+            let avg = 0;
+            arr.forEach(item => {
+                avg+=parseInt(item);
+            })
+            avg /= arr.length;
+            if(avg >= 10.7){
+                console.log(`You will have stipend ${avg}`)
+            }
+            else{
+                console.log(`You will not have stipend ${avg}`)
+            }
+            break;
+    
+        default:
+            temp = false;
+            break;
+    }
+}*/
+
+// 2
+
+
+
+// 3
+
+
+
+
